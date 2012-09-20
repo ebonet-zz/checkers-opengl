aed3304dc43ee6f258a5b323893e0dce2224824a
caiovgb@gmail.com <caiovgb@gmail.com@4a9c08c1-fbd8-e27c-37ba-cf935eff7f04>
2012-09-20 07:36:49 +0000

    Fixing project

--- Checkers/src/coordinate.cpp
+++ Checkers/src/coordinate.cpp
@@ -1,6 +1,4 @@
 /*
-
-
  * Coordinate.cpp
  *
  *  Created on: Sep 19, 2012
@@ -9,25 +7,25 @@
 #include "coordinate.h"
 using namespace std;
 
-coordinate::coordinate(int row, int column){
-	this->row=row;
+coordinate::coordinate(int row, int column) {
+	this->row = row;
 	this->column = column;
 }
 
-coordinate::coordinate(){
-	this->column=-1;
-	this->row=-1;
+coordinate::coordinate() {
+	this->column = -1;
+	this->row = -1;
 }
 
-int coordinate::rowDistance(Coordinate anotherCoordinate){
-	return abs(this->row-anotherCoordinate.row);
+int coordinate::rowDistance(Coordinate anotherCoordinate) {
+	return abs(this->row - anotherCoordinate.row);
 }
 
-int coordinate::columnDistance(Coordinate anotherCoordinate){
-	return abs(this->column-anotherCoordinate.column);
+int coordinate::columnDistance(Coordinate anotherCoordinate) {
+	return abs(this->column - anotherCoordinate.column);
 }
 
-bool coordinate::isDiagonalNeighbor(Coordinate anotherCoordinate){
-	return this->rowDistance(anotherCoordinate)==1 && this->columnDistance(anotherCoordinate)==1;
+bool coordinate::isDiagonalNeighbor(Coordinate anotherCoordinate) {
+	return this->rowDistance(anotherCoordinate) == 1 && this->columnDistance(anotherCoordinate) == 1;
 }
 
--- Checkers/src/gameGrid.cpp
+++ Checkers/src/gameGrid.cpp
@@ -5,7 +5,6 @@
  *      Author: BONET
  */
 
-#include <cmath>
 #include <string.h>
 #include <iostream>
 #include <fstream>
@@ -15,37 +14,36 @@
 #include "coordinate.h"
 using namespace std;
 
-
-GameGrid::GameGrid(){
+GameGrid::GameGrid() {
 	createInitialGrid();
 }
 
-void GameGrid::restart(){
+void GameGrid::restart() {
 	createInitialGrid();
 }
-char GameGrid::getCellAt(Coordinate A){
+char GameGrid::getCellAt(Coordinate A) {
 	return GameGridMatrix[A.row][A.column];
 }
 
-char GameGrid::getCellAt(int row,int column){
+char GameGrid::getCellAt(int row, int column) {
 	return GameGridMatrix[row][column];
 }
 
-bool GameGrid::isEmptyCell(Coordinate A){
+bool GameGrid::isEmptyCell(Coordinate A) {
 	return getCellAt(A) == EMPTY_CELL;
 }
 
-bool GameGrid::isPiece(Coordinate A){
-	return (getCellAt(A)==BEIGE_PAWN || getCellAt(A)==RED_PAWN);
+bool GameGrid::isPiece(Coordinate A) {
+	return (getCellAt(A) == BEIGE_PAWN || getCellAt(A) == RED_PAWN);
 }
 
-void GameGrid::movePiece(Coordinate A, Coordinate B){
-	if(!isEmptyCell(B)){
-		cout<< "Invalid movement: target is not empty" <<endl;
+void GameGrid::movePiece(Coordinate A, Coordinate B) {
+	if (!isEmptyCell(B)) {
+		cout << "Invalid movement: target is not empty" << endl;
 		return;
 	}
-	if(!isPiece(A)){
-		cout<< "Invalid movement: source is not a piece" <<endl;
+	if (!isPiece(A)) {
+		cout << "Invalid movement: source is not a piece" << endl;
 		return;
 	}
 
@@ -53,49 +51,47 @@ void GameGrid::movePiece(Coordinate A, Coordinate B){
 	GameGridMatrix[A.row][A.column] = EMPTY_CELL;
 }
 
-void GameGrid::removePieceAt(Coordinate A){
-	if(getCellAt(A)==VOID_CELL){
-		cout << "Illegal move: tried to remove piece from void cell" <<endl;
+void GameGrid::removePieceAt(Coordinate A) {
+	if (getCellAt(A) == VOID_CELL) {
+		cout << "Illegal move: tried to remove piece from void cell" << endl;
 		return;
 	}
 
 	GameGridMatrix[A.row][A.column] = EMPTY_CELL;
 }
 
-
-
-bool GameGrid::isEnemyPiece(Coordinate A, Coordinate B){
-	return (getCellAt(A) =='r' && getCellAt(B) == 'b') || (getCellAt(A) =='b' && getCellAt(B) == 'r');
+bool GameGrid::isEnemyPiece(Coordinate A, Coordinate B) {
+	return (getCellAt(A) == 'r' && getCellAt(B) == 'b') || (getCellAt(A) == 'b' && getCellAt(B) == 'r');
 }
 
-void GameGrid::createGridFromFile(char * s){
+void GameGrid::createGridFromFile(char * s) {
 	FILE * pFile;
 	int c;
-	int row = 0,column=0;
+	int row = 0, column = 0;
 
-	pFile=fopen (s,"r");
+	pFile = fopen(s, "r");
 
-	if (pFile==NULL) perror ("Error opening file");
-	else
-	{
+	if (pFile == NULL)
+		perror("Error opening file");
+	else {
 		do {
-			c = fgetc (pFile);
-			if(c=='\n'){
+			c = fgetc(pFile);
+			if (c == '\n') {
 				row++;
-				column=0;
+				column = 0;
 				continue;
 			}
-			GameGridMatrix[row][column]=c;
+			GameGridMatrix[row][column] = c;
 			column++;
 
 		} while (c != EOF);
 		fclose (pFile);
 	}
 
-	return ;
+	return;
 }
 
-void GameGrid::createInitialGrid(){
+void GameGrid::createInitialGrid() {
 	createGridFromFile("InitialGameGrid.txt");
 }
 
